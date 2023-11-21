b4045e8151b23830f417618846482a722907bd44
Mohammad Salah <83545789+MOSALAH-CODE@users.noreply.github.com>
2023-11-21 16:24:01 +0300

    Add fade-up entrance transition to Card component
    Implemented a fade-up entrance animation for the Card component using CSS transitions. Added classes 'card-unmounted' and 'card-mounted' to control opacity and transform properties, triggering a smooth transition when the component mounts. Adjusted timeout to delay mounting and ensure a seamless animation effect. Updated CSS styles for transition properties and applied conditional rendering to handle header and footer elements.

--- resources/js/Components/Card.tsx
+++ resources/js/Components/Card.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactNode } from "react";
+import { PropsWithChildren, ReactNode, useEffect, useState } from "react";
 
 const Card = ({
     children,
@@ -10,12 +10,30 @@ const Card = ({
     footer?: ReactNode;
     divider?: boolean;
 }>) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        // Use a timeout to delay setting `isMounted` to true
+        const timeout = setTimeout(() => {
+            setIsMounted(true);
+        }, 500);
+
+        // Clear timeout on component unmount or cleanup
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
-        <div className="p-4 bg-white rounded-lg shadow-sm">
+        <div
+            className={`p-4 bg-white rounded-lg shadow-sm ${
+                isMounted ? "card-mounted" : "card-unmounted"
+            }`}
+        >
             {header ? (
                 <header>
                     <div
-                        className={`grid grid-cols-1 ${divider && "divide-y"}`}
+                        className={`grid grid-cols-1 ${
+                            divider ? "divide-y" : ""
+                        }`}
                     >
                         <div className="pb-2">{header}</div>
                         <div>{children}</div>
